09ab4d22f25d12707b6558584eafb77340125bb7
Finn <fnn.flts@yahoo.de>
2018-12-28 15:28:43 +0100

    Projektdateien hinzufügen.

---
+++ CountDown_Silvester/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace CountDown_Silvester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lCountdownHours = new System.Windows.Forms.Label();
+            this.timerCountdown = new System.Windows.Forms.Timer(this.components);
+            this.labelLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.lCountdownSeconds = new System.Windows.Forms.Label();
+            this.lCountdownN = new System.Windows.Forms.Label();
+            this.lCountdownN2 = new System.Windows.Forms.Label();
+            this.lCountdownMinutes = new System.Windows.Forms.Label();
+            this.labelLayout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 0;
+            // 
+            // lCountdownHours
+            // 
+            this.lCountdownHours.AutoSize = true;
+            this.lCountdownHours.Location = new System.Drawing.Point(265, 210);
+            this.lCountdownHours.Name = "lCountdownHours";
+            this.lCountdownHours.Size = new System.Drawing.Size(36, 25);
+            this.lCountdownHours.TabIndex = 1;
+            this.lCountdownHours.Text = "12";
+            // 
+            // timerCountdown
+            // 
+            this.timerCountdown.Interval = 1000;
+            this.timerCountdown.Tick += new System.EventHandler(this.timerCountdown_Tick);
+            // 
+            // labelLayout
+            // 
+            this.labelLayout.ColumnCount = 5;
+            this.labelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.labelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.labelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.labelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.labelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 52F));
+            this.labelLayout.Controls.Add(this.lCountdownSeconds, 4, 0);
+            this.labelLayout.Controls.Add(this.lCountdownN, 1, 0);
+            this.labelLayout.Controls.Add(this.lCountdownN2, 3, 0);
+            this.labelLayout.Controls.Add(this.lCountdownMinutes, 2, 0);
+            this.labelLayout.Location = new System.Drawing.Point(841, 538);
+            this.labelLayout.Name = "labelLayout";
+            this.labelLayout.RowCount = 1;
+            this.labelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.labelLayout.Size = new System.Drawing.Size(601, 240);
+            this.labelLayout.TabIndex = 7;
+            this.labelLayout.Visible = false;
+            // 
+            // lCountdownSeconds
+            // 
+            this.lCountdownSeconds.AutoSize = true;
+            this.lCountdownSeconds.Location = new System.Drawing.Point(552, 0);
+            this.lCountdownSeconds.Name = "lCountdownSeconds";
+            this.lCountdownSeconds.Size = new System.Drawing.Size(36, 25);
+            this.lCountdownSeconds.TabIndex = 5;
+            this.lCountdownSeconds.Text = "12";
+            // 
+            // lCountdownN
+            // 
+            this.lCountdownN.AutoSize = true;
+            this.lCountdownN.Location = new System.Drawing.Point(447, 0);
+            this.lCountdownN.Name = "lCountdownN";
+            this.lCountdownN.Size = new System.Drawing.Size(18, 25);
+            this.lCountdownN.TabIndex = 2;
+            this.lCountdownN.Text = ":";
+            // 
+            // lCountdownN2
+            // 
+            this.lCountdownN2.AutoSize = true;
+            this.lCountdownN2.Location = new System.Drawing.Point(525, 0);
+            this.lCountdownN2.Name = "lCountdownN2";
+            this.lCountdownN2.Size = new System.Drawing.Size(18, 25);
+            this.lCountdownN2.TabIndex = 4;
+            this.lCountdownN2.Text = ":";
+            // 
+            // lCountdownMinutes
+            // 
+            this.lCountdownMinutes.AutoSize = true;
+            this.lCountdownMinutes.Location = new System.Drawing.Point(475, 0);
+            this.lCountdownMinutes.Name = "lCountdownMinutes";
+            this.lCountdownMinutes.Size = new System.Drawing.Size(36, 25);
+            this.lCountdownMinutes.TabIndex = 3;
+            this.lCountdownMinutes.Text = "12";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1681, 813);
+            this.Controls.Add(this.labelLayout);
+            this.Controls.Add(this.lCountdownHours);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.labelLayout.ResumeLayout(false);
+            this.labelLayout.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lCountdownHours;
+        private System.Windows.Forms.Timer timerCountdown;
+        private System.Windows.Forms.TableLayoutPanel labelLayout;
+        private System.Windows.Forms.Label lCountdownSeconds;
+        private System.Windows.Forms.Label lCountdownN;
+        private System.Windows.Forms.Label lCountdownN2;
+        private System.Windows.Forms.Label lCountdownMinutes;
+    }
+}
+
---
+++ CountDown_Silvester/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace CountDown_Silvester
+{
+    public partial class Form1 : Form
+    {
+        #region Variables
+        System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+        private string soundLocation = "C:/Users/Zocke/Music/Panic! At The Disco - High Hopes.wav";
+        private string sSoundLength = "0:3:16";
+        private int iSoundLength = 0;
+
+        private int iCurrentHours = 0, iCurrentMinutes = 0, iCurrentSeconds = 0;
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Layout();
+
+            player.SoundLocation = soundLocation;
+            iSoundLength = ConvertTime_PlayMusic(sSoundLength);
+
+            timerCountdown_Tick(sender, e);
+            timerCountdown.Start();
+        }
+        
+        private new void Layout()
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+            Form1.ActiveForm.BackColor = Color.Black;
+            /*
+            labelLayout.AutoSize = false;
+            labelLayout.Dock = DockStyle.Fill;
+            labelLayout.Anchor = AnchorStyles.None;
+            */
+
+            ///*
+            lCountdownHours.AutoSize = false;
+            lCountdownHours.TextAlign = ContentAlignment.MiddleCenter;
+            lCountdownHours.Dock = DockStyle.Fill;
+            lCountdownHours.Font = new Font("Microsoft Sans Serif", 150, FontStyle.Bold);
+            //*/
+            //lCountdownHours.Dock = DockStyle.Fill;
+            lCountdownHours.ForeColor = Color.LightGray;
+
+            /*lCountdownMinutes.AutoSize = false;
+            lCountdownMinutes.TextAlign = ContentAlignment.MiddleCenter;
+            lCountdownMinutes.Dock = DockStyle.Fill;
+            lCountdownMinutes.Font = new Font("Microsoft Sans Serif", 150, FontStyle.Bold);
+            */
+            lCountdownMinutes.Dock = DockStyle.Fill;
+            lCountdownMinutes.ForeColor = Color.LightGray;
+
+            /*lCountdownSeconds.AutoSize = false;
+            lCountdownSeconds.TextAlign = ContentAlignment.MiddleCenter;
+            lCountdownSeconds.Dock = DockStyle.Fill;
+            lCountdownSeconds.Font = new Font("Microsoft Sans Serif", 150, FontStyle.Bold);
+            */
+            lCountdownSeconds.Dock = DockStyle.Fill;
+            lCountdownSeconds.ForeColor = Color.LightGray;
+        }
+
+        private void timerCountdown_Tick(object sender, EventArgs e)
+        {
+            GetCurrentTime();
+            lCountdownHours.Text = GetLeftTime();
+            PlayMusic();
+        }
+
+        private void Log()
+        {
+            Console.WriteLine("-------------------------------");
+            //Console.WriteLine("Uhrzeit gerade: " + GetCurrentHours() + ":" + GetCurrentMinutes() + ":" + GetCurrentSeconds());
+            //Console.WriteLine("Zeit bis 24 Uhr: " + GetLeftHours() + ":" + GetLeftMinutes() + ":" + GetLeftSeconds());
+            //Console.WriteLine("Zeit in Sekunden: " + ((GetLeftHours() * 60 * 60) + (GetLeftMinutes() * 60) + GetLeftSeconds()));
+            //Console.WriteLine("Sekunden von ConvertTime: " + (86400 - ConvertTime_PlayMusic("12:12:30")));
+        }
+
+        #region getter & setter
+        private void GetCurrentTime()
+        {
+            string time = DateTime.Now.TimeOfDay.ToString();
+            string[] timeSplit = time.Split(':');
+            int i = 0;
+            foreach (string word in timeSplit)
+            {
+                i++;
+                switch (i)
+                {
+                    default:
+                        return;
+                    case 1:
+                        Int32.TryParse(word, out iCurrentHours);
+                        break;
+                    case 2:
+                        Int32.TryParse(word, out iCurrentMinutes);
+                        break;
+                    case 3:
+                        string[] sSeconds = word.Split('.');
+                        int[] iSeconds = new int[2];
+                        Int32.TryParse(sSeconds[0], out iSeconds[0]);
+                        Int32.TryParse(sSeconds[1], out iSeconds[1]);
+                        if (iSeconds[1] < 5)
+                            iCurrentSeconds = iSeconds[0];
+                        else if(iSeconds[1] >= 5)
+                            iCurrentSeconds = iSeconds[0] + 1;
+                        break;
+                }
+            }
+        }
+
+        private string GetLeftTime()
+        {
+            int hours = 24, minutes = 60, seconds = 60;
+            return  (hours - GetCurrentHours() - 1).ToString() + ":" + 
+                    (minutes - GetCurrentMinutes() - 1).ToString() + ":" +
+                    (seconds - GetCurrentSeconds()).ToString();
+        }
+
+        private int GetCurrentHours()
+        {
+            return iCurrentHours;
+        }
+
+        private int GetCurrentMinutes()
+        {
+            return iCurrentMinutes;
+        }
+
+        private int GetCurrentSeconds()
+        {
+            return iCurrentSeconds;
+        }
+
+        private int GetLeftHours()
+        {
+            return (24 - iCurrentHours - 1);
+        }
+
+        private int GetLeftMinutes()
+        {
+            return (60 - iCurrentMinutes - 1);
+        }
+
+        private int GetLeftSeconds()
+        {
+            return (60 - iCurrentSeconds);
+        }
+        #endregion
+
+        #region stop & play song
+        private void PlayMusic()
+        {
+            if (((GetLeftHours() * 60 * 60) + (GetLeftMinutes() * 60) + GetLeftSeconds()) == (86400 - ConvertTime_PlayMusic("12:16:0"))) //86400 = 24h
+                PlaySound();
+        }
+
+        private void PlaySound()
+        {
+            player.Play();
+        }
+
+        private int ConvertTime_PlayMusic(string time)
+        {
+            string[] asTime = time.Split(':');
+            int[] aiTime = new int[asTime.Length];
+            Int32.TryParse(asTime[0], out aiTime[0]);
+            Int32.TryParse(asTime[1], out aiTime[1]);
+            Int32.TryParse(asTime[2], out aiTime[2]);
+            return (aiTime[0] * 60 * 60) + (aiTime[1] * 60) + aiTime[2];
+        }
+        #endregion
+    }
+}
